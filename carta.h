2b837c13897b8e9e4cd9ee9a96af697b33048c90
nombre <81451283+nombre15@users.noreply.github.com>
2024-01-02 16:40:29 -0500

    Cambios estructurales

--- carta.h
+++ carta.h
@@ -7,16 +7,16 @@ class carta
 {
     public:
         Carta();
-        void tomarCartas(int& suma, int& dealerSuma);
-        void mostrarResultado(int& suma, int& dealerSuma, int& dinero, char& tecla, bool &finalizado);
+        void tomarCartas(int& jugadorSuma, int& dealerSuma);
+        void mostrarResultado(int jugadorSuma, int dealerSuma, int dinero, char& tecla, bool &finalizado);
 
     private:
         string letra[4] {"A", "J", "Q", "K"};
         string simbolo[4] {" de diamantes", " de corazones", " de trebol", " de picas"};
 };
 
-void jugadorGana(int& dinero, int& suma);
-void jugadorPierde(int& dinero, int& suma);
-int ingresarTecla(char tecla, int dinero);
+//Metodos del main que seran usados en carta.cpp
+void jugadorGana(int& dinero);
+void jugadorPierde(int& dinero);
 
 #endif // CARTA_H
